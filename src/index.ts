55aa899f30ebe603b8f06bac83ce655aff9b0ce6
Eric <blade.eric@gmail.com>
2020-11-13 22:58:00 -0500

    add sandbox regions, marketplace ids
    - first real successful test, getAuthorizationCode fails, but it fails
      from the correct requested region!

--- src/index.ts
+++ src/index.ts
@@ -4,16 +4,41 @@ import type { default as apid } from './api-types';
 /* @ts-ignore */ // ignore the next line so that the code that builds the import file can be compiled
 const spec = (await import('./sp-api-swagger.json')).default;
 
-enum ApiRegion {
+export enum ApiRegion {
     NorthAmerica = 'na',
+    NorthAmericaSandbox = 'sandbox-na',
     Europe = 'eu',
+    EuropeSandbox = 'sandbox-eu',
     FarEast = 'fe',
+    FarEastSandbox = 'sandbox-fe',
 };
 
 const RegionServers: Record<ApiRegion, { awsRegion: string, endpoint: string }> = {
     na: { awsRegion: 'us-east-1', endpoint: 'sellingpartnerapi-na.amazon.com' },
+    'sandbox-na': { awsRegion: 'us-east-1', endpoint: 'sandbox.sellingpartnerapi-na.amazon.com' },
     eu: { awsRegion: 'eu-west-1', endpoint: 'sellingpartnerapi-eu.amazon.com' },
+    'sandbox-eu': { awsRegion: 'eu-west-1', endpoint: 'sandbox.sellingpartnerapi-eu.amazon.com' },
     fe: { awsRegion: 'us-west-2', endpoint: 'sellingpartnerapi-fe.amazon.com' },
+    'sandbox-fe': { awsRegion: 'us-west-2', endpoint: 'sandbox.sellingpartnerapi-fe.amazon.com' },
+};
+
+export enum Marketplace {
+    CA = 'A2EUQ1WTGCTBG2',
+    US = 'ATVPDKIKX0DER',
+    MX = 'A1AM78C64UM0Y8',
+    BR = 'A2Q3Y263D00KWC',
+    ES = 'A1RKKUPIHCS9HS',
+    GB = 'A1F83G8C2ARO7P',
+    FR = 'A13V1IB3VIYZZH',
+    NL = 'A1805IZSGTT6HS',
+    DE = 'A1PA6795UKMFR9',
+    IT = 'APJ6JRA9NG5V4',
+    TR = 'A33AVAJ2PDY3EV',
+    AE = 'A2VIGQ35RCS4UG',
+    IN = 'A21TJRUUN4KGV',
+    SG = 'A19VAU5U5O7RUS',
+    AU = 'A39IBJ37TRP1C6',
+    JP = 'A1VC38T7YXB528',
 };
 
 type getAuthorizationCodeParams = {
@@ -51,7 +76,7 @@ export default class SpApi {
     }
 }
 
-const test = new SpApi({ region: ApiRegion.FarEast });
+const test = new SpApi({ region: ApiRegion.NorthAmericaSandbox });
 const x = await test.getAuthorizationCode({developerId: '1',mwsAuthToken: '1',sellingPartnerId:'1'})
 /* const { apis: {
     authorization,
